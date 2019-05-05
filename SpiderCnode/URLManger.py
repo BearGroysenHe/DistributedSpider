24e3789be493bfc52f8027252835c0444dd81051
BearGroysenHe <heshaopo@hotmail.com>
2019-05-05 17:49:28 +0800

    Debug节点控制器

--- SpiderCnode/URLManger.py
+++ SpiderCnode/URLManger.py
@@ -7,32 +7,23 @@ class UrlManger(object):
         self.old_urls = self.load_progress('old_urls.txt')
 
     def has_new_url(self):
-        return self.new_url_size() != 0
+        return len(self.new_urls) != 0
 
     def get_new_url(self):
         new_url = self.new_urls.pop()
         m = hashlib.md5()
-        m.update(new_url)
+        m.update(new_url.encode('utf-8'))
         self.old_urls.add(m.hexdigest()[8:-8])
         return new_url
-
-    def add_new_url(self,url):
-        if url is None:
-            return
-        m = hashlib.md5()
-        m.update(url)
-        url_md5 = m.hexdigest()[8:-8]
-        if url not in self.new_urls and url_md5 not in self.old_urls:
-            self.new_urls.add(url)
 
     def add_new_urls(self,urls):
         if urls is None or len(urls) == 0:
             return
         for url in urls:
-            self.add_new_url(url)
-
-    def new_url_size(self):
-        return len(self.new_urls)
+            m = hashlib.md5()
+            url_md5 = m.update(url.encode('utf-8'))
+            if url not in self.new_urls and url_md5 not in self.old_urls:
+                self.new_urls.add(url)
 
     def old_url_size(self):
         return len(self.old_urls)
